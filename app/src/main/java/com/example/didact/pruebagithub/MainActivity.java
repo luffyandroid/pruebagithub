e3786485c8ccbcca30095d811fd94f1db14930f4
luisjesus <luisjesus.delgado89@gmail.com>
2018-03-02 13:33:39 +0100

    gfrdgfdfs

--- app/src/main/java/com/example/didact/pruebagithub/MainActivity.java
+++ app/src/main/java/com/example/didact/pruebagithub/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.didact.pruebagithub;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void transicionDesdeDerecha (View view){
+
+        Intent i = new Intent(getApplicationContext(),EstiloActivity.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.left_in, R.anim.left_out);
+    }
+
 }
